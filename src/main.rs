75550f2af31921db8915e8b2296394a8a40fbc64
Hayashi Takui <hayashi.azi1mov@outlook.com>
2023-05-25 13:16:45 +0700

    Make db init read url from .env

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate diesel;
 use actix_web::{error, get, web, middleware, App, HttpResponse, HttpServer, Responder, Result};
 use diesel::{prelude::*, r2d2};
 
+
 mod actions;
 mod models;
 mod schema;
@@ -62,7 +63,8 @@ async fn main() -> std::io::Result<()> {
 }
 
 fn initialize_db_pool() -> DbPool {
-    let manager = r2d2::ConnectionManager::<SqliteConnection>::new("/data/sqlite.db");
+    let conn_spec = std::env::var("DATABASE_URL").expect("DATABASE_URL should be set");
+    let manager = r2d2::ConnectionManager::<SqliteConnection>::new(conn_spec);
     r2d2::Pool::builder()
         .build(manager)
         .expect("database URL should be valid path to SQLite DB file")
